4ad19e3a632e7976f4d78aea6d19fed4ede6316a
bilal-shah968 <bilalshahs965@gmail.com>
2024-11-19 13:15:48 +0500

    Automapper

--- CloudQuick/Configurations/AutoMapperConfig.cs
+++ CloudQuick/Configurations/AutoMapperConfig.cs
@@ -8,10 +8,19 @@ namespace CloudQuick.Configurations
     {
         public AutoMapperConfig()
         {
-              // CreateMap<Student, StudentDTo>();
-               //CreateMap<StudentDTo, Student>();
+            // CreateMap<Student, StudentDTo>();
+            //CreateMap<StudentDTo, Student>();
 
+            //Config for different property names 
+            //CreateMap<StudentDTo, Student>().ReverseMap().ForMember(n => n.Name, opt => opt.MapFrom(x => x.StudentName));
+
+            //Confiq for ignorring some property names
+            //CreateMap<StudentDTo, Student>().ReverseMap().ForMember(n => n.StudentName, opt => opt.Ignore());
+
+            //Confiq for Transforming some property names
+            //CreateMap<StudentDTo, Student>().ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No adress found" : n);
             CreateMap<StudentDTo, Student>().ReverseMap();
+
         }
     }
 }
